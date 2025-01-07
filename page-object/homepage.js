7769bca254f8164368d4d5bf3f22b367971ce750
Imran Hasan <imranhasanraaz@gmail.com>
2025-01-07 21:48:30 +0600

    Update main nav locator

--- page-object/homepage.js
+++ page-object/homepage.js
@@ -8,7 +8,7 @@ export const interestedProduct = '.nextiva-custom-select'
 export const productName = 'li[data-value="CX (CI)"]'
 export const submit = '.nextiva-ct-button'
 export const banner = '.announcement-banner'
-export const menu = '.main-nav'
+export const menu = '.main-navigation'
 export const getStartedButton = '#cta-popup'
 export const footer = "//button[@class='nextiva-footer-download']"
 export const sales = "//li[@class='support-link-item']/a[small[text()='Sales']]/span"
